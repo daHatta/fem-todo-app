bc362550637e0a44cfe363f4f50e6031f02238dc
Heiko John <healjo@hotmail.com>
2024-03-28 23:50:36 +0100

    New todo and delete todo implemented

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,41 @@ import iconCross from "./images/icon-cross.svg";
 
 export default function Home() {
 
-  const [todoList, setTodoList] = useState([]);
+  const [newToDo, setNewToDo] = useState("");
+  const [toDoList, setToDoList] = useState([
+    { id: "1", content: "Item 1"},
+    { id: "2", content: "Item 2"},
+    { id: "3", content: "Item 3"},
+  ]);
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 9999);
+  }
+
+  const handleKeyUp = (key: string) => {
+    if (key === "Enter" && newToDo) {
+      const randomNumber = getRandomNumber();
+
+      const newItem = {
+        id: `item-${randomNumber}`,
+        content: newToDo,
+      }
+
+      setToDoList(toDoList.concat(newItem));
+
+      setNewToDo("");
+    }
+  }
+
+  const handleDelete = (id: number) => {
+    if (id > -1) {
+      setToDoList(toDoList.slice(0, id).concat(toDoList.slice(id+1)))
+    }
+  }
 
   return (
     <main>
-      <div className="addToDo flex gap-x-[10px] justify-center items-center w-full p-[12px] rounded-[6px]">
+      <div className="newToDo flex gap-x-[10px] justify-center items-center w-full p-[12px] rounded-[6px]">
         <div>
           <label>
             <input type="checkbox" disabled />
@@ -19,54 +49,41 @@ export default function Home() {
           </label>
         </div>
         <div className="w-full">
-          <input type="text" placeholder="Create a new todo..." />
+          <input type="text" id="newToDo" value={newToDo} onChange={(e) => setNewToDo(e.target.value)} onKeyUp={(e) => handleKeyUp(e.key)} placeholder="Create a new todo..." />
         </div>
       </div>
-
+      
       <ul className="mt-[16px]">
-        <li className="toDo flex flex-row justify-center items-center gap-x-[10px] p-[12px]">
-          <div>
-            <label>
-              <input type="checkbox" />
-              <span className="visually-hidden">Checkbox</span>
-            </label>
-          </div>
-          <div className="flex flex-row justify-between items-center gap-x-[10px] w-full">
-            <input type="text" placeholder="Create a new todo..." className="w-full" />
-            <button type="button">
-              <Image 
-                src={iconCross}
-                width={12}
-                height={12}
-                alt="Icon Cross"
-              />
-            </button>
-          </div>
-        </li>
-        <li className="toDo flex flex-row justify-center items-center gap-x-[10px] p-[12px]">
-          <div>
-            <label>
-              <input type="checkbox" />
-              <span className="visually-hidden">Checkbox</span>
-            </label>
-          </div>
-          <div className="flex flex-row justify-between items-center gap-x-[10px] w-full">
-            <input type="text" placeholder="Create a new todo..." className="w-full" />
-            <button type="button">
-              <Image 
-                src={iconCross}
-                width={12}
-                height={12}
-                alt="Icon Cross"
-              />
-            </button>
-          </div>
-        </li>
+        {
+          toDoList?.map((item, index) => {
+            return (
+              <li key={item.id} className="toDo flex flex-row justify-center items-center gap-x-[10px] p-[12px]">
+                <div>
+                  <label htmlFor={`${index}`}>
+                    <input id={`${index}`} type="checkbox" />
+                    <span className="visually-hidden">Checkbox</span>
+                  </label>
+                </div>
+                <div className="flex flex-row justify-between items-center gap-x-[10px] w-full">
+                  <input type="text" value={item.content} placeholder="Create a new todo..." className="w-full" />
+                  <button type="button" onClick={() => handleDelete(index)}>
+                    <Image 
+                      src={iconCross}
+                      width={12}
+                      height={12}
+                      alt="Icon Cross"
+                    />
+                  </button>
+                </div>
+              </li>
+            )
+          })
+        }
       </ul>
 
       <div className="grid grid-areas-slim gap-y-[15px] drop-shadow-lg 2xl:grid-areas-wide">
         <div className="counter flex justify-start p-[17px] rounded-bl-lg">
-          <span>5 items left</span>
+          <span>{toDoList.length} items left</span>
         </div>
         <div className="options flex justify-center gap-[20px] p-[17px] rounded-[6px] 2xl:rounded-none">
           <Link href={"/"}>All</Link>
