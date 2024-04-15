de1dbfab581c1073ad1de2bb0e34b31d07ad8fa7
Heiko John <healjo@hotmail.com>
2024-04-15 22:44:20 +0200

    Notice css added

--- app/components/Notice/index.tsx
+++ app/components/Notice/index.tsx
@@ -1,8 +1,11 @@
+import classes from "./index.module.css";
 
 const Notice = () => {
-    return (
-        <p className="text-center pt-[43px]">Drag and drop to reorder list</p>
-    ) 
-}
+  return (
+    <p className={`${classes.notice} text-center pt-[43px]`}>
+      Drag and drop to reorder list
+    </p>
+  );
+};
 
-export default Notice
+export default Notice;
